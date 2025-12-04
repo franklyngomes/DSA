59de2dd8ee1ba640f910575d2b7e4f33372616f5
franklyngomes <franklyngomes15@gmail.com>
2025-12-04 15:38:18 +0530

    Patterns

--- src/Patterns.java
+++ src/Patterns.java
@@ -4,115 +4,222 @@ public class Patterns {
 //        ****
 //        ****
 //        ****
-        for(int i = 1; i < 5; i ++){
-            for(int j = 1; j < 5; j ++){
-            System.out.print("*");
-            }
-            System.out.println();
-        }
-
+//        for(int i = 0; i < 4; i ++ ){
+//            for (int j = 0 ; j < 5; j ++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 //        *****
 //        *   *
 //        *   *
 //        *****
-        int n = 4;
-        int m = 5;
-        for(int i = 1; i <= n; i ++){
-            for (int j = 1; j <= m; j ++){
-                if(i == 1 || j == 1 || i == n || j == m){
-                System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-
+//        for(int i = 1; i <=4; i ++){
+//            for(int j = 1; j <= 5; j ++){
+//                if(i == 1 || j == 1 || i == 4 || j == 5){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 //        *
 //        **
 //        ***
 //        ****
-        for(int i = 1; i <= 4; i ++){
-            for(int j = 1 ; j <= i; j ++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
+//        for(int i = 1; i <= 4; i ++){
+//            for(int j = 1; j <= i ; j ++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 //        ****
 //        ***
 //        **
 //        *
-        for (int i = 1; i <= 4; i++) {
-            for (int j = i; j <= 4; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-//        *
-//       **
-//      ***
-//     ****
-        for(int i = 1; i <= 4 ; i ++){
-            for(int j = 1; j <= 4-i; j ++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= i; j ++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for(int i = 4; i >= 1; i --){
+//            for(int j = 1; j <= i ; j ++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//         *
+//        **
+//       ***
+//      ****
+//        for(int i = 1; i <=4; i ++){
+////            Spaces
+//            for(int j = 1; j <= 4-i; j ++){
+//                System.out.print(" ");
+//            }
+////            Stars
+//            for(int j = 1; j <= i; j ++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 //        1
 //        12
 //        123
 //        1234
 //        12345
-        for(int i = 1; i <= 5; i ++){
-            for(int j = 1; j <= i; j ++){
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+//        for(int i = 1; i <= 5; i ++){
+//            for(int j = 1; j <= i ; j ++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
 //        12345
 //        1234
 //        123
 //        12
 //        1
-        for(int i = 1; i <= 5; i ++){
-            for(int j = 1; j <= 5-i+1; j ++){
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-        //Floyds Triangle
+//        for(int i = 5; i >= 1; i --){
+//            for(int j = 1; j <= i ; j ++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
 //        1
-//        2 3
-//        4 5 6
-//        7 8 9 10
+//        2  3
+//        4  5  6
+//        7  8  9  10
 //        11 12 13 14 15
-        int n = 5;
-        int number = 1;
-        for(int i = 1; i <= n; i ++){
-            for(int j = 1; j <= i ; j ++){
-                System.out.print(number + " ");
-                number++;
-            }
-            System.out.println();
-        }
+//        int number = 1;
+//        for(int i = 1; i <= 5; i ++){
+//            for (int j = 1; j <= i; j ++){
+//                System.out.print(number + " ");
+//                number ++;
+//            }
+//            System.out.println();
+//        }
 //        1
 //        01
 //        101
 //        0101
 //        10101
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                if ((i + j) % 2 == 0) {
-                    System.out.print(1);
-                } else {
-                    System.out.print(0);
-                }
+//        for(int i = 1; i <= 5; i ++){
+//            for(int j = 1; j <=i; j ++){
+//                int sum = i + j;
+//                if(sum % 2 == 0){
+//                    System.out.print(1);
+//                }else{
+//                    System.out.print(0);
+//                }
+//            }
+//            System.out.println();
+//        }
+//        *****
+//       *****
+//      *****
+//     *****
+//    *****
+//        for(int i = 5; i >=1 ; i --){
+//            for(int j = 1; j <= i; j ++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <= 5; j ++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        1
+//        22
+//       333
+//      44444
+//     555555
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5 - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+//        1
+//       212
+//      32123
+//     4321234
+//    5432122345
+//        for(int i = 1; i <= 5; i ++){
+//            for(int j = 1; j <= 5 - i; j ++){
+//                System.out.print(" ");
+//            }
+//            for(int j = i; j >= 1; j --){
+//                System.out.print(j);
+//            }
+//            for(int j = 2; j <= i; j ++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//        *      *
+//        **    **
+//        ***  ***
+//        ********
+//        ********
+//        ***  ***
+//        **    **
+//        *      *
+//        for (int i = 1; i <= 4; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            for (int j = 1; j <= 2 * (4 - i); j++) {
+//                System.out.print(" " + " ");
+//            }
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 4; i >= 1; i--) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            for (int j = 1; j <= 2 * (4 - i); j++) {
+//                System.out.print(" " + " ");
+//            }
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*" + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        *
+//       ***
+//      *****
+//     *******
+//     *******
+//      *****
+//       ***
+//        *
+        for(int i = 1; i <= 4; i ++){
+            for(int j = 3; j >= i; j --){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j ++){
+                System.out.print("*");
+            }
+            for(int j = 2; j <= i; j ++){
+                System.out.print("*");
             }
             System.out.println();
         }
+        for(int i = 4; i >= 1; i --){
+            for(int j = 3; j >= i; j --){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j ++){
+                System.out.print("*");
+            }
+            for(int j = 2; j <= i; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
--- src/Variables.java
+++ src/Variables.java
@@ -3,20 +3,22 @@ import java.util.*;
 public class Variables {
     public static void main(String[] args){
         //variables
-        String name = "Tony Stark";
-        int a = 25;
-        int b = 10;
-        float price = 25.25f;
-        b = 20;
-        name = "Franklyn";
-
-        int sum = a + b;
-        int pro = a * b;
-        System.out.println(pro);
+//        String name = "Tony Stark";
+//        int a = 25;
+//        int b = 10;
+//        float price = 25.25f;
+//        b = 20;
+//        name = "Franklyn";
+//
+//        int sum = a + b;
+//        int pro = a * b;
+//        System.out.println(pro);
 
         //Input
-        Scanner sc = new Scanner(System.in);
-        String myName = sc.next();
-        System.out.println(name);
+//        Scanner sc = new Scanner(System.in);
+//        String myName = sc.next();
+//        System.out.println(name);
+
+        System.out.println(args[0]);
     }
 }
